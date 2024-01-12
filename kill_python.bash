6b57e6fb8399ae29c90530494d6db996367a970e
gdvstd <iblitzrod0324@gmail.com>
2024-01-12 12:44:53 +0000

    update kill_python

--- kill_python.bash
+++ kill_python.bash
@@ -8,25 +8,29 @@ if [ -n "$python_processes" ]; then
     echo "Python processes found:"
     ps -p $python_processes #-o pid,cmd
 
-    echo -e "\nSpecify the PID(s) of the Python process(es) to kill:"
+    echo -e "\nSpecify the PID(s) of the Python process(es) to kill (comma-separated), or enter 'exit' to end:"
     
     read user_input
     
-    # Kill specific Python processes based on user input
-    IFS=',' read -ra pids <<< "$user_input"
-    for pid in "${pids[@]}"; do
-        if [[ "$pid" =~ ^[0-9]+$ ]]; then
-            # Check if the PID is valid
-            if ps -p $pid > /dev/null; then
-                kill -9 $pid
-                echo "Python process with PID $pid killed."
+    if [ "$user_input" == "exit" ]; then
+        exit
+    else
+        # Kill specific Python processes based on user input
+        IFS=',' read -ra pids <<< "$user_input"
+        for pid in "${pids[@]}"; do
+            if [[ "$pid" =~ ^[0-9]+$ ]]; then
+                # Check if the PID is valid
+                if ps -p $pid > /dev/null; then
+                    kill -9 $pid
+                    echo "Python process with PID $pid killed."
+                else
+                    echo "Invalid PID: $pid. No action taken."
+                fi
             else
-                echo "Invalid PID: $pid. No action taken."
+                echo "Invalid input: $pid. Please enter a valid PID or 'all'."
             fi
-        else
-            echo "Invalid input: $pid. Please enter a valid PID or 'all'."
-        fi
-    done
+        done
+    fi
 else
     echo "No Python processes found."
 fi
