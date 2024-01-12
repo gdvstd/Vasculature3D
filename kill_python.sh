32470fd856160498a412448307f0b9e7b1f2fe61
sehyunNam <iblitzrod0324@gmail.com>
2024-01-12 17:01:54 +0900

    add kill python process

---
+++ kill_python.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Find Python processes
+python_processes=$(pgrep -x "python")
+
+# Check if any Python processes are running
+if [ -n "$python_processes" ]; then
+    echo "Python processes found:"
+    ps -p $python_processes #-o pid,cmd
+
+    echo -e "\nSpecify the PID(s) of the Python process(es) to kill (comma-separated), or enter 'all' to kill all:"
+    
+    read user_input
+    
+    if [ "$user_input" == "all" ]; then
+        # Kill all Python processes
+        kill -9 $python_processes
+        echo "All Python processes killed."
+    else
+        # Kill specific Python processes based on user input
+        IFS=',' read -ra pids <<< "$user_input"
+        for pid in "${pids[@]}"; do
+            if [[ "$pid" =~ ^[0-9]+$ ]]; then
+                # Check if the PID is valid
+                if ps -p $pid > /dev/null; then
+                    kill -9 $pid
+                    echo "Python process with PID $pid killed."
+                else
+                    echo "Invalid PID: $pid. No action taken."
+                fi
+            else
+                echo "Invalid input: $pid. Please enter a valid PID or 'all'."
+            fi
+        done
+    fi
+else
+    echo "No Python processes found."
+fi
+
